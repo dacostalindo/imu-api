f09058cbd872ef8b71f0069b798f1c502921fda7
dacostalindo <mali3819@colorado.edu>
2020-03-10 21:16:11 -0600

    changes

--- test_imu_api.py
+++ test_imu_api.py
@@ -224,7 +224,7 @@ class TestIMUAPI(unittest.TestCase):
         module = 'general_data'
         field = 'all'
         # print(self.imu.read_telemetry_items(module=module,fields=fields))
-        self.imu._read_telemetry_items()
+        self.imu.read_telemetry()
 
 
         # fields = [field]
